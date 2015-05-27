73474c597d0e0edceca3eb8f9c9e3214200835ce
georgechan32 <georgechan32@gmail.com>
2015-05-27 02:28:38 -0700

    Added save and beep
    yays?
    still a lot left to do though

--- admin_client.py
+++ admin_client.py
@@ -3,7 +3,6 @@ import sys
 from threading import Thread
 from time import sleep
 
-
 #Model
 class Client(Handler):
     
--- client.py
+++ client.py
@@ -3,45 +3,69 @@ import sys
 from threading import Thread
 from time import sleep
 
-myname = raw_input("What is your name? ")
-
-#Model
-class Client(Handler):
-    
-    #def on_open(self):
-
-
-    def on_close(self):
-        #pass
-        client.do_send("ClientClosed")  #ehh...
-        print "Client Closing..."
-
-    def on_msg(self, msg):
-        print msg
-
-        
-host, port = 'localhost', 8888
-client = Client(host, port)
-client.do_send({'join': "Me"})
-#client.on_msg('yes')
 #View
+def beep():
+    print "\a"
+def getInput():
+    mytemptxt = raw_input("Me " + ' >> ')
+    return mytemptxt
+def print_msg(msg):
+    print msg
+    global stringSoFar
+    stringSoFar = stringSoFar + str(msg) + "\n"
 
+#Controller
+class Client(Handler):
+    def on_close(self):
+        pass
+    def on_msg(self, msg):
+        print_msg(msg)
 def periodic_poll():
     while 1:
         poll()
         sleep(0.05)  # seconds
-                            
+def parseSubject():
+    if len(topic2)>20:
+        topic2[:20]
+    topic2.replace(" ", "_")
+
+def main_funct():
+    v_continue = True 
+    while v_continue:
+        mytxt = getInput()
+        if mytxt == ":q":
+            client.do_close()
+            v_continue = False
+        elif mytxt == ":e":
+            beep()
+        elif mytxt == ":s":
+            file_wr = open(topic2+".txt", 'w+')
+            file_wr.write(stringSoFar)
+            file_wr.close()
+        else:
+            client.do_send("Me" + '|' + mytxt)
+            global stringSoFar
+            stringSoFar = "Me: " + stringSoFar + mytxt+ "\n"
+
+
+#Model
+                           
 thread = Thread(target=periodic_poll)
 thread.daemon = True  # die when the main thread dies 
 thread.start()
+topic1 = raw_input("Please select your topic\n 1: Feedback\t2: Complaint\t3: Misc")
+topic2 = raw_input("What is the name of the topic? (20 character limit): ")
+host, port = 'localhost', 8888
+client = Client(host, port)
+client.do_send({'join': "Me"})
+client.do_send("Me|TOPIC: ("+topic1 +") "+topic2 )
+stringSoFar = ""
+#client.on_msg('yes')
+parseSubject()
+main_funct()
 
-v_continue = True
 
-#Controller
-while v_continue:
-    mytxt = raw_input(myname + ' >> ')
-    if mytxt == ":q":
-        client.do_close()
-        v_continue = False
-    else:
-        client.do_send("Me" + '|' + mytxt)
+
+
+
+
